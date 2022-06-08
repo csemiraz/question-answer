0af0c390ee97d896442e33c6dedc09e95ded491e
csemiraz <mirazul2013@gmail.com>
2022-06-08 12:39:42 +0600

    add author info and question creation date in question item

--- resources/views/front-end/questions/index.blade.php
+++ resources/views/front-end/questions/index.blade.php
@@ -9,7 +9,10 @@
             <div class="col-md-12 mb-2">
                 <div class="card">
                     <div class="card-body">
-                      <h5 class="card-title">{{ $question->title }}</h5>
+                      <h5 class="card-title"><a href="{{ route('questions.show', $question->id) }}">{{ $question->title }}</a></h5>
+                      <p>Asked by <a href="#">{{ $question->user->name }}</a>
+                        <small class="text-muted">{{ $question->created_at->diffForHumans() }}</small>  
+                      </p>
                       <p class="card-text">{{ Str::limit($question->body, 250) }}</p>
                       {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
                     </div>
